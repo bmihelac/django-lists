1955d67af109b2624ffe995adfa619476c607696
Bojan Mihelac <bmihelac@mihelac.org>
2013-01-10 14:29:24 +0100

    Make ItemForm `folder_name` HiddenInput

--- lists/forms.py
+++ lists/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class ItemForm(forms.Form):
-    folder_name = forms.CharField(required=False)
+    folder_name = forms.CharField(required=False,
+            widget=forms.widgets.HiddenInput)
     content_type = forms.CharField(widget=forms.widgets.HiddenInput)
     object_id = forms.CharField(widget=forms.widgets.HiddenInput)
